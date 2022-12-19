f6c9f9206dd6e0c2264661f807fbbf1ed6d6bf90
ghazyayman <muhghazyayman@gmail.com>
2022-12-19 12:31:46 +0800

    listings

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings');
 });
-Route::get('/hello', function() {
-    return respones('Hello World');
-}); 
