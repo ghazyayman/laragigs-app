0f7ea6d0301544b11f9ae32946a665c5b2fb6d1c
ghazyayman <muhghazyayman@gmail.com>
2022-12-20 22:59:06 +0800

    fix

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -4,7 +4,7 @@
 
 @foreach($listings as $listing)
 <h2>
-    <a href="/listings/{{$listing['id]}}">{{$listing['title']}}</a>
+    <a href="/listings/{{$listing['id']}}">{{$listing['title']}}</a>
 </h2>
 <p>
     {{$listing['description']}}
