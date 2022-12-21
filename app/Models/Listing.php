516530269905545664c6879de3995aca1aa97c27
ghazyayman <muhghazyayman@gmail.com>
2022-12-21 13:04:58 +0800

    eloquent model

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,35 +1,11 @@
 <?php
+
 namespace App\Models;
 
-    class Listing {
-        public static function all() {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit. Morbi cursus risus at enim vulputate, 
-                    quis volutpat ante suscipit. Cras consequat gravida dui vitae lacinia. 
-                    Cras ac fermentum massa. Sed ornare pretium purus at egestas.'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit. Morbi cursus risus at enim vulputate, 
-                    quis volutpat ante suscipit. Cras consequat gravida dui vitae lacinia. 
-                    Cras ac fermentum massa. Sed ornare pretium purus at egestas.'
-            ]
-        ];
-        }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-        public static function find($id) {
-            $listings = self::all();
-            
-            foreach($listings as $listing){
-                if($listing['id'] == $id) {
-                    return $listing;
-                }
-            }
-        }
-    } 
+class Listing extends Model
+{
+    use HasFactory;
+}
--- database/migrations/2022_12_20_155125_create_listings_table.php
+++ database/migrations/2022_12_20_155125_create_listings_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('listings', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->string('title'); 
             $table->string('tags');
             $table->string('company');
             $table->string('location');
