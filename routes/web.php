55e51b959ec438c3ae864c142924f5600a917ac8
ghazyayman <muhghazyayman@gmail.com>
2022-12-29 15:11:20 +0800

    blade components

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,9 @@ Route::get('/', function () {
 
 
 // Single listing 
-Route::get('/listings/{id}', function ($id) {
+Route::get('/listings/{listing}', function(Listing
+$listing) {
     return view('listing', [
-        'listing' => Listing::find($id)
+        'listing' => $listing
     ]);
 });
