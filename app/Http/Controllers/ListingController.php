def609a69465ab991be7dc179f6bd4086dd890df
ghazyayman <muhghazyayman@gmail.com>
2023-01-06 00:02:24 +0800

    finish

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -43,6 +43,8 @@ class ListingController extends Controller
             $formFields['logo'] = $request->file('logo')->store('logos', 'public');
         }
 
+        $formFields['user_id'] = auth()->id();
+
         Listing::create($formFields);
 
         return redirect('/')->with('message', 'Listing created successfully!');
@@ -59,6 +61,12 @@ class ListingController extends Controller
     // Update Listing Data
     public function update(Request $request, Listing $listing) 
     {
+
+        // Make sure logged in user is the owner of the listing
+        if($listing->user_id != auth()->id()) {
+            abort(403, 'Unoauthorized action');
+        }
+
         $formFields = $request->validate([
             'title' =>'required',
             'company' => ['required'],
@@ -81,8 +89,19 @@ class ListingController extends Controller
 
     // Delete Listing
     public function destroy(Listing $listing) {
+        // Make sure logged in user is the owner of the listing
+        if($listing->user_id != auth()->id()) {
+            abort(403, 'Unoauthorized action');
+        }
+        
         $listing->delete();
         return redirect('/')->with('message', 'Listing deleted successfully!');
     }
 
+    // Manage Listings
+    public function manage() {
+        return view('listings.manage', [
+            'listings' => auth()->user()->listings()->get()
+        ]);
+    }
 }    
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
@@ -15,10 +16,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(5)->create();
+        // \App\Models\User::factory(5)->create();
 
+        $user = User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'john@gmail.com'
+        ]);
 
-        Listing::factory(6)->create();
+        Listing::factory(6)->create([
+            'user_id' => $user->id
+        ]);
 
         // Listing::create(
         //     [
